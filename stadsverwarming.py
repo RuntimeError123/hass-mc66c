505ec9440329d9bbf3d5beb0b88950c770ec09fd
RuntimeError123 <lennaert.bosch@outlook.com>
2018-02-03 13:41:16 +0100

    Icons, protocol, port, variable, harmonizing
    Icons were statically defined. Added as variables
    Protocol was statically defined. Added as variable.
    Port was staticaly defined. Added as variable.
    Also did some harmonizing work. Changed " to ' at several locations.

--- stadsverwarming.py
+++ stadsverwarming.py
@@ -27,17 +27,23 @@
 
 # Variables
 #################################
-serialport = "/dev/ttyUSB13"
-homeassistant_ip = "192.168.1.X"
-homeassistant_password = "password"
-energy_entity_id = "sensor.stadsverwarming_energy"
-energy_friendly_name = "Stadsverwarming energie"
-volume_entity_id = "sensor.stadsverwarming_volume"
-volume_friendly_name = "Stadsverwarming volume"
-temp_in_entity_id = "sensor.stadsverwarming_temperature_in"
-temp_in_friendly_name = "Stadsverwarming inkomend"
-temp_out_entity_id = "sensor.stadsverwarming_temperature_out"
-temp_out_friendly_name = "Stadsverwarming uitgaand"
+serialport = '/dev/ttyUSB13'
+protocol = 'http'
+homeassistant_ip = '192.168.1.X'
+port = '8123'
+homeassistant_password = 'password'
+energy_entity_id = 'sensor.stadsverwarming_energy'
+energy_friendly_name = 'Stadsverwarming energie'
+energy_icon = 'mdi:gauge'
+volume_entity_id = 'sensor.stadsverwarming_volume'
+volume_friendly_name = 'Stadsverwarming volume'
+volume_icon = 'mdi:water'
+temp_in_entity_id = 'sensor.stadsverwarming_temperature_in'
+temp_in_friendly_name = 'Stadsverwarming inkomend'
+temp_in_icon = 'mdi:thermometer'
+temp_out_entity_id = 'sensor.stadsverwarming_temperature_out'
+temp_out_friendly_name = 'Stadsverwarming uitgaand'
+temp_out_icon = 'mdi:thermometer'
 
 # Importing modules
 #################################
@@ -49,7 +55,7 @@ from time import sleep
 # Change entity state function
 #################################
 def change_entity_state(entity_id: str, friendly_name: str, unit_of_measurement: str, icon: str, new_state: float):
-	url = 'http://'+homeassistant_ip+':8123/api/states/'+entity_id
+	url = protocol+'://'+homeassistant_ip+':'+port+'/api/states/'+entity_id
 	headers = {'x-ha-access' : homeassistant_password ,'content-type' : 'application/json'}
 	data = json.dumps({'state' : new_state,'attributes' : {'unit_of_measurement' : unit_of_measurement,'friendly_name' : friendly_name, 'icon' : icon}})
 	response = requests.post(url, headers=headers, data=data)
@@ -76,11 +82,11 @@ new_temp_out = int((new_data[4]).decode('utf-8'))/100
 
 # Updating HomeAssistant values
 ###############################
-energy_response = change_entity_state(energy_entity_id, energy_friendly_name,"GJ","mdi:gauge",new_energy)
-print("Energy updated: ",energy_response)
-volume_response = change_entity_state(volume_entity_id, volume_friendly_name,"M3","mdi:water",new_volume)
-print("Energy updated: ",volume_response)
-temp_in_response = change_entity_state(temp_in_entity_id,temp_in_friendly_name,"°C","mdi:thermometer",new_temp_in)
-print("Energy updated: ",temp_in_response)
-temp_out_response = change_entity_state(temp_out_entity_id,temp_out_friendly_name,"°C","mdi:thermometer",new_temp_out)
-print("Energy updated: ",temp_out_response)
+energy_response = change_entity_state(energy_entity_id, energy_friendly_name,'GJ',energy_icon,new_energy)
+print('Energy updated: ',energy_response)
+volume_response = change_entity_state(volume_entity_id, volume_friendly_name,'M3',volume_icon,new_volume)
+print('Energy updated: ',volume_response)
+temp_in_response = change_entity_state(temp_in_entity_id,temp_in_friendly_name,'°C',temp_in_icon,new_temp_in)
+print('Energy updated: ',temp_in_response)
+temp_out_response = change_entity_state(temp_out_entity_id,temp_out_friendly_name,'°C',temp_out_icon,new_temp_out)
+print('Energy updated: ',temp_out_response)
